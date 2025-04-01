c1a0063442b6855662ab9e89288568038a4cadab
Paolo Carnevali <pacarnev@ucsc.edu>
2025-04-01 09:14:41 -0700

    Better display of the tangle matrix.

--- src/TangleMatrix.cpp
+++ src/TangleMatrix.cpp
@@ -74,76 +74,66 @@ void TangleMatrix::writeHtml(
     const AssemblyGraph& assemblyGraph,
     ostream& html) const
 {
-    html << "<h2>Tangle matrix</h2>";
-
-
-
-    // Entrances.
     html <<
-        "<h3>Entrances</h3>"
-        "<p><table>"
-        "<tr><th>Index<th>Segment<th>AnchorId<th>Coverage";
-    for(uint64_t iEntrance=0; iEntrance<entrances.size(); iEntrance++) {
-        const Entrance& entrance = entrances[iEntrance];
-        const edge_descriptor e = entrance.e;
-        const AnchorId anchorId = entrance.anchorId;
-        const uint64_t coverage = assemblyGraph.anchors[anchorId].coverage();
-        html <<
-            "<tr>"
-            "<td class = centered>" << iEntrance <<
-            "<td class = centered>" << assemblyGraph[e].id <<
-            "<td class = centered>" << anchorIdToString(anchorId) <<
-            "<td class = centered>" << coverage;
+        "<h2>Tangle matrix</h2>"
+        "<p>"
+        "<table>"
+        "<tr><td rowspan=6 colspan=6>"
+        "<th colspan=" << (exits.size() + 1) << " style='background-color:LightCyan'>Exits";
+
+    html << "<tr><th style='background-color:LightCyan'>Index";
+    for(uint64_t i=0; i<exits.size(); i++) {
+        html << "<td class=centered style='background-color:LightCyan'>" << i;
     }
-    html << "</table>";
 
+    html << "<tr><th style='background-color:LightCyan'>Segment";
+    for(uint64_t i=0; i<exits.size(); i++) {
+        html << "<td class=centered style='background-color:LightCyan'>" << assemblyGraph[exits[i].e].id;
+    }
 
+    html << "<tr><th style='background-color:LightCyan'>AnchorId";
+    for(uint64_t i=0; i<exits.size(); i++) {
+        html << "<td class=centered style='background-color:LightCyan'>" << anchorIdToString(exits[i].anchorId);
+    }
 
-    // Exits.
+    html << "<tr><th style='background-color:LightCyan'>Coverage";
+    for(uint64_t i=0; i<exits.size(); i++) {
+        html << "<td class=centered style='background-color:LightCyan'>" << exits[i].coverage;
+    }
+
+    html << "<tr><th style='background-color:LightCyan'>Common<br>coverage";
+    for(uint64_t i=0; i<exits.size(); i++) {
+        html << "<td class=centered style='background-color:LightCyan'>" << exits[i].commonCoverage;
+    }
+
     html <<
-        "<h3>Exits</h3>"
-        "<p><table>"
-        "<tr><th>Index<th>Segment<th>AnchorId<th>Coverage";
-    for(uint64_t iExit=0; iExit<exits.size(); iExit++) {
-        const Exit& exit = exits[iExit];
-        const edge_descriptor e = exit.e;
-        const AnchorId anchorId = exit.anchorId;
-        const uint64_t coverage = assemblyGraph.anchors[anchorId].coverage();
-        html <<
-            "<tr>"
-            "<td class = centered>" << iExit <<
-            "<td class = centered>" << assemblyGraph[e].id <<
-            "<td class = centered>" << anchorIdToString(anchorId) <<
-            "<td class = centered>" << coverage;
-    }
-    html << "</table>";
+        "<tr><th rowspan=" << entrances.size() + 1 <<
+        " style='background-color:CornSilk'>Entrances"
+        "<th style='background-color:CornSilk'>Index"
+        "<th style='background-color:CornSilk'>Segment"
+        "<th style='background-color:CornSilk'>AnchorId"
+        "<th style='background-color:CornSilk'>Coverage"
+        "<th style='background-color:CornSilk'>Common<br>coverage<td>";
 
-
-
-    // Tangle matrix.
-    html <<
-        "<h3>Tangle matrix</h3>"
-        "<p>One row for each entrance, one column for each exit."
-        "<p><table>"
-        "<tr><td rowspan=2 colspan=2>";
-    for(uint64_t iExit=0; iExit<exits.size(); iExit++) {
-        html << "<th class=centered>" << iExit;
+    for(uint64_t i=0; i<entrances.size(); i++) {
+        html << "<td>";
     }
-    html << "<tr>";
-    for(uint64_t iExit=0; iExit<exits.size(); iExit++) {
-        const Exit& exit = exits[iExit];
-        html << "<th class=centered>" << assemblyGraph[exit.e].id;
-    }
+
     for(uint64_t iEntrance=0; iEntrance<entrances.size(); iEntrance++) {
         const Entrance& entrance = entrances[iEntrance];
         html <<
             "<tr>"
-            "<th class=centered>" << iEntrance <<
-            "<th class=centered>" << assemblyGraph[entrance.e].id;
+            "<td class=centered style='background-color:CornSilk'>" << iEntrance <<
+            "<td class=centered style='background-color:CornSilk'>" << assemblyGraph[entrance.e].id <<
+            "<td class=centered style='background-color:CornSilk'>" << anchorIdToString(entrance.anchorId) <<
+            "<td class=centered style='background-color:CornSilk'>" << entrance.coverage <<
+            "<td class=centered style='background-color:CornSilk'>" << entrance.commonCoverage <<
+            "<td>";
         for(uint64_t iExit=0; iExit<exits.size(); iExit++) {
-            html << "<td class=centered>" << tangleMatrix[iEntrance][iExit];
+            html << "<td class=centered style='background-color:LightPink'>" << tangleMatrix[iEntrance][iExit];
         }
     }
+
     html << "</table>";
 }
 
