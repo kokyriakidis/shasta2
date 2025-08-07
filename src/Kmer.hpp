81fc386ea8ae042f6edaa96966601c6e221757bc
Paolo Carnevali <pacarnev@ucsc.edu>
2025-08-07 08:38:08 -0700

    Remove some unused definitions..

--- src/Kmer.hpp
+++ src/Kmer.hpp
@@ -6,10 +6,6 @@ namespace shasta {
 
     // Type used to represent a k-mer.
     // This limits the maximum k-mer length that can be used.
-    using Kmer16 = ShortBaseSequence16;
-    using Kmer32 = ShortBaseSequence32;
-    using Kmer64 = ShortBaseSequence64;
-
     using Kmer128 = ShortBaseSequence128;
     using Kmer = Kmer128;
     static_assert(Kmer::capacity == 128, "Unexpected Kmer capacity.");
