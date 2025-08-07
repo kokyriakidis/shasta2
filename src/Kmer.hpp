d9ca9890c697e2d0f9c00c757eaea350c50562d5
Paolo Carnevali <pacarnev@ucsc.edu>
2025-08-07 08:35:24 -0700

    Remove the definition of KmerId.

--- src/Kmer.hpp
+++ src/Kmer.hpp
@@ -1,46 +1,19 @@
-#ifndef SHASTA_KMER_HPP
-#define SHASTA_KMER_HPP
+#pragma once
 
-#include "BitCounter.hpp"
-#include "shastaTypes.hpp"
 #include "ShortBaseSequence.hpp"
 
 namespace shasta {
 
     // Type used to represent a k-mer.
     // This limits the maximum k-mer length that can be used.
-    // If this changes, KmerId must also be changed.
     using Kmer16 = ShortBaseSequence16;
     using Kmer32 = ShortBaseSequence32;
     using Kmer64 = ShortBaseSequence64;
 
-#if 1
-
-    // 128-base markers.
     using Kmer128 = ShortBaseSequence128;
     using Kmer = Kmer128;
-    static_assert(
-        BitCounter<KmerId>::numberOfBits == 256,
-        "Unexpected number of bits in KmerId.");
-    static_assert(
-        Kmer::capacity == 128,
-        "Unexpected Kmer capacity.");
+    static_assert(Kmer::capacity == 128, "Unexpected Kmer capacity.");
 
-#else
-
-    // 64-base markers.
-    using Kmer = Kmer64;
-    static_assert(
-        BitCounter<KmerId>::numberOfBits == 128,
-        "Unexpected number of bits in KmerId.");
-    static_assert(
-        Kmer::capacity == 64,
-        "Unexpected Kmer capacity.");
-#endif
-
-    static_assert(
-        BitCounter<KmerId>::numberOfBits == 2 * Kmer::capacity,
-        "Kmer and KmerId types are inconsistent.");
 }
 
-#endif
+
--- src/shastaTypes.hpp
+++ src/shastaTypes.hpp
@@ -1,34 +1,10 @@
 #pragma once
 
-#if 1
-// For 128-base markers (the KmerId is 256 bits).
-#include <boost/multiprecision/cpp_int.hpp>
-#endif
-
 #include <cstdint.hpp>
 
 namespace shasta {
-
-    // A KmerId requires twice the number of bits of the integer type
-    // used to define ShortBaseSequence.
-    using KmerId16 = uint32_t;
-    using KmerId32 = uint64_t;
-    using KmerId64 = __uint128_t;
-
-#if 1
-
-    // 128-base markers.
-    using KmerId128 = boost::multiprecision::uint256_t;
-    using KmerId = KmerId128;
-#else
-
-    // 64-base markers.
-    using KmerId = KmerId64;
-#endif
-
     using ReadId = uint32_t;
     using Strand = ReadId;
-
     using AnchorId = uint64_t;
 }
 
