e5e328ae4107cab0aa7f6553f6d9149be9020026
Paolo Carnevali <pacarnev@ucsc.edu>
2025-09-11 09:52:43 -0700

    Remove some unused code.

--- src/SuperbubbleChain.hpp
+++ src/SuperbubbleChain.hpp
@@ -13,9 +13,6 @@ public:
     using vertex_descriptor = AssemblyGraph::vertex_descriptor;
     using edge_descriptor = AssemblyGraph::edge_descriptor;
 
-    uint64_t phase(AssemblyGraph&, uint64_t superbubbleChainId);
-
-    // Version that uses Tangle1/TangleMatrix1 instead of Tangle/TangleMatrix.
     uint64_t phase1(
         AssemblyGraph&,
         uint64_t superbubbleChainId,
