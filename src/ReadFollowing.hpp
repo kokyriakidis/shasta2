ececf1cba065dd16833ef37a65fb10ae3c6fae78
Paolo Carnevali <pacarnev@ucsc.edu>
2025-10-02 16:18:26 -0700

    Remove some unused code.

--- src/ReadFollowing.hpp
+++ src/ReadFollowing.hpp
@@ -33,57 +33,6 @@ private:
     using AVertexPair = AssemblyGraphVertexPair;
     using AEdgePair = AssemblyGraphEdgePair;
 
-#if 0
-    // A "line graph" representation of the AssemblyGraph.
-    // Names or vertices and edges of the line graph are prefixed with "l".
-    // Each vertex of the LineGraph corresponds to an edge of the AssemblyGraph.
-    // Given LineGraph vertices lv0 and lv1 corresponding to AssemblyGraph
-    // edges e0 and e1, a LineGraph edge lv0->lv1 is created if
-    // target(e0, assemblyGraph) == source(e1, assemblyGraph).
-    // The LineGraph is similar to the Bandage representation of the AssemblyGraph.
-    using LineGraph = boost::adjacency_list<
-        boost::vecS,
-        boost::vecS,
-        boost::bidirectionalS,
-        AEdge>;
-    LineGraph lineGraph;
-    std::map<AEdge, LineGraph::vertex_descriptor> lineGraphVertexMap;
-    void createLineGraph();
-
-
-
-    // A filtered version of the LineGraph that contains
-    // only the vertices corresponding to some AssemblyGraph edges.
-    // This is not permanently stored. It is only created as needed.
-    class FilteringPredicate {
-    public:
-        FilteringPredicate(
-            const LineGraph* lineGraph = 0,
-            const std::set<AEdge>* assemblyGraphEdges = 0) :
-            lineGraph(lineGraph),
-            assemblyGraphEdges(assemblyGraphEdges)
-        {}
-        const LineGraph* lineGraph;
-        const std::set<AEdge>* assemblyGraphEdges;
-
-        bool operator()(const LineGraph::vertex_descriptor& lv) const
-        {
-            const AEdge ae = (*lineGraph)[lv];
-            return assemblyGraphEdges->contains(ae);
-        }
-
-        bool operator()(const LineGraph::edge_descriptor& le) const
-        {
-            const LineGraph::vertex_descriptor lv0 = source(le, *lineGraph);
-            const LineGraph::vertex_descriptor lv1 = target(le, *lineGraph);
-            const AEdge ae0 = (*lineGraph)[lv0];
-            const AEdge ae1 = (*lineGraph)[lv1];
-            return assemblyGraphEdges->contains(ae0) and assemblyGraphEdges->contains(ae1);
-        }
-    };
-    using FilteredLineGraph = boost::filtered_graph<LineGraph, FilteringPredicate, FilteringPredicate>;
-#endif
-
 
 
     // Find appearances of OrientedReadIds in the initial/final
