b3afa791338a2d301dcb8a66e306d7b9121aa6ce
Paolo Carnevali <pacarnev@ucsc.edu>
2026-01-12 08:42:56 -0800

    Remove AssemblyGraph::search which is not used.

--- src/AssemblyGraph.hpp
+++ src/AssemblyGraph.hpp
@@ -353,29 +353,6 @@ public:
         vector<OrientedReadEdgeInfo>&) const;
 
 
-    // Simple search starting at a given edge (segment) and moving in the specified direction.
-    void search(
-        edge_descriptor,
-        uint64_t direction) const;
-    class SearchGraphVertex {
-    public:
-        AssemblyGraph::edge_descriptor e;
-        SearchGraphVertex(AssemblyGraph::edge_descriptor e) : e(e) {}
-    };
-    class SearchGraphEdge {
-    public:
-        uint64_t tangleMatrix;
-    };
-    class SearchGraph : public boost::adjacency_list<
-        boost::listS,
-        boost::listS,
-        boost::bidirectionalS,
-        SearchGraphVertex,
-        SearchGraphEdge> {
-        public:
-    };
-
-
 
     // Superbubbles and SuperbubbleChains.
     // - A Superbubble is a Tangle in which all entrance edges are into
