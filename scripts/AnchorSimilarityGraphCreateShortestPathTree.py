64a0965b42070a53f3dd55654f1e1d5b7bdaf86c
Paolo Carnevali <pacarnev@ucsc.edu>
2026-05-04 09:41:19 -0700

    Add forgotten files.

---
+++ scripts/AnchorSimilarityGraphCreateShortestPathTree.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import shasta2
+
+# Get the AnchorId.
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('anchorId', type=str)
+arguments = parser.parse_args()
+anchorIdString = arguments.anchorId
+anchorId = shasta2.anchorIdFromString(anchorIdString);
+
+# Access what we need.
+assembler = shasta2.Assembler()
+assembler.accessMarkers()
+assembler.accessAnchors()
+assembler.accessAnchorSimilarityGraph()
+
+# Call the shortest path code.
+assembler.anchorSimilarityGraphCreateShortestPathTree(anchorId)
