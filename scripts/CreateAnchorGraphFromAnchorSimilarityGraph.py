43b5e575d9347d47789befd2f3a4636dc87d58a4
Paolo Carnevali <pacarnev@ucsc.edu>
2026-05-15 06:56:18 -0700

    Add missing file.

---
+++ scripts/CreateAnchorGraphFromAnchorSimilarityGraph.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import shasta2
+
+# Access what we need.
+assembler = shasta2.Assembler()
+assembler.accessMarkers()
+assembler.accessMarkerKmers()
+assembler.accessAnchors()
+assembler.accessAnchorSimilarityGraph()
+
+# Call the shortest path code.
+assembler.createAnchorGraphFromAnchorSimilarityGraph()
+assembler.saveAnchorGraph()
