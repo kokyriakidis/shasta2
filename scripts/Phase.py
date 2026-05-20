55d7a3b1df977379acf0d4755fd229286a95cf68
Paolo Carnevali <pacarnev@ucsc.edu>
2026-05-20 07:32:24 -0700

    Script to phase a given assembly stage.

---
+++ scripts/Phase.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+import shasta2
+
+# Get the argument.
+import argparse
+parser = argparse.ArgumentParser(description = "Do a round oh phasing on a given assembly stage.")
+parser.add_argument("stage", type=str, help="Assembly stage.")
+arguments = parser.parse_args()
+
+shasta2.openPerformanceLog("Python-performance.log")
+
+# Get the options from shasta2.conf.
+options = shasta2.Options()
+
+# Create the Assembler and access what we need.
+assembler = shasta2.Assembler()
+assembler.accessMarkers()
+assembler.accessAnchors()
+assembler.accessJourneys()
+
+# Load the specified assembly stage and assemble sequence.
+assemblyGraph = assembler.getAssemblyGraph(arguments.stage, options)
+assemblyGraph.phaseSuperbubbleChains()
+
+# Write it out.
+assembledName = arguments.stage + "-Phased"
+assemblyGraph.write(assembledName)
+
+
+
+
