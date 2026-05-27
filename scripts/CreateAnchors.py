9b8ba2875ef85ae7f22c7888b4c7f571582390c7
Paolo Carnevali <pacarnev@ucsc.edu>
2026-05-27 12:07:22 -0700

    Add missibg argument.

--- scripts/CreateAnchors.py
+++ scripts/CreateAnchors.py
@@ -7,5 +7,10 @@ options = shasta2.Options()
 assembler = shasta2.Assembler()
 assembler.accessMarkers()
 assembler.accessMarkerKmers()
-assembler.createAnchors(options.minAnchorCoverage, options.maxAnchorCoverage, options.maxAnchorRepeatLength, 0)
+assembler.createAnchors(
+    options.minAnchorCoverage, 
+    options.maxAnchorCoverage, 
+    options.maxAnchorRepeatLength, 
+    options.minAnchorDistinctSubkmerCount,
+    0)
 
