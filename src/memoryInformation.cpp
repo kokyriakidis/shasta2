8a6ffbbf750c0f085e5040bf0d025a825f63d6aa
Paolo Carnevali <pacarnev@ucsc.edu>
2026-06-03 11:57:18 -0700

    Adds getMallocInfo. More comprehensive version of writeMemoryStatistics.

--- src/memoryInformation.cpp
+++ src/memoryInformation.cpp
@@ -1,12 +1,24 @@
+// Shasta2.
 #include "memoryInformation.hpp"
 #include "performanceLog.hpp"
 #include "timestamp.hpp"
 using namespace shasta2;
 
+// Boost libraries.
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/info_parser.hpp>
+#include <boost/property_tree/xml_parser.hpp>
+
+// Linux.
+#include <malloc.h>
+#include <stdio.h>
+
+// Standard library.
 #include "fstream.hpp"
 #include "iostream.hpp"
-#include <sstream>
+#include "stdexcept.hpp"
 #include "string.hpp"
+#include <sstream>
 
 
 
@@ -67,6 +79,93 @@ VirtualMemoryInfo shasta2::getVirtualMemoryInfo()
 
 
 
+void MallocInfo::write(ostream& s) const
+{
+    s << "Heap size " << heapSize << "\n";
+    s << "Heap used " << heapUsed() << "\n";
+    s << "Heap free " << heapFree << "\n";
+    s << "Maximum heap size " << maxHeapSize << "\n";
+    s << "Mmap size " << mmapSize << "\n";
+    s << flush;
+}
+
+
+
+MallocInfo shasta2::getMallocInfo()
+{
+
+    // Create a FILE* for use with malloc_info.
+    char* buffer = 0;
+    uint64_t bufferSize = 0;
+    FILE* file = ::open_memstream(&buffer, &bufferSize);
+    if(not file) {
+        throw runtime_error("Error in open_memstream.");
+    }
+
+    // Call malloc_info.
+    const int returnCode = ::malloc_info(0, file);
+    if(returnCode == -1) {
+        throw runtime_error("Error in malloc_info.");
+    }
+    ::fclose(file);
+
+    // Create a string to use to create the property tree.
+    // We could use a string_view but c++23
+    // would not support that constructor.
+    const string s(buffer, bufferSize);
+    std::istringstream is(s);
+    // cout << s << flush;
+
+    // Create the property tree.
+    boost::property_tree::ptree propertyTree;
+    boost::property_tree::read_xml(is, propertyTree);
+    ::free(buffer);
+
+    // boost::property_tree::write_info(cout, propertyTree);
+
+    // Extract the information we want.
+    MallocInfo mallocInfo;
+    for(const auto& a: propertyTree) {
+        for(const auto& b: a.second) {
+
+            if(b.first == "total") {
+                for(const auto& c: b.second) {
+                    const string type = c.second.get<string>("type");
+                    // cout << "total type " << c.second.get<string>("type") << endl;
+                    // cout << "total count " << c.second.get<uint64_t>("count") << endl;
+                    // cout << "total size " << c.second.get<uint64_t>("size") << endl;
+                    if((type == "fast") or (type == "rest")) {
+                        mallocInfo.heapFree += c.second.get<uint64_t>("size");
+                    }
+                    if(type == "mmap") {
+                        mallocInfo.mmapSize += c.second.get<uint64_t>("size");
+                    }
+                }
+            }
+
+            if(b.first == "system") {
+                for(const auto& c: b.second) {
+                    const string type = c.second.get<string>("type");
+                    // cout << "system type " << c.second.get<string>("type") << endl;
+                    // cout << "system size " << c.second.get<uint64_t>("size") << endl;
+                    if(type == "current") {
+                        mallocInfo.heapSize = c.second.get<uint64_t>("size");
+                    }
+                    if(type == "max") {
+                        mallocInfo.maxHeapSize = c.second.get<uint64_t>("size");
+                    }
+                }
+            }
+        }
+    }
+
+    return mallocInfo;
+
+}
+
+
+
+
 // Get total physical memory available, in bytes.
 uint64_t shasta2::getTotalPhysicalMemory()
 {
@@ -81,10 +180,14 @@ uint64_t shasta2::getTotalPhysicalMemory()
 
 void shasta2::writeMemoryStatistics(const string& name)
 {
+    const VirtualMemoryInfo virtualMemoryInfo = getVirtualMemoryInfo();
+
     performanceLog <<
-        timestamp << "At " << name << ":\n" <<
-        "    Peak virtual memory usage " << getPeakMemoryUsage() << "\n" <<
-        flush;
+        timestamp <<
+        "At " << name << ":\n" <<
+        "Virtual memory (current) " << virtualMemoryInfo.current << "\n" <<
+        "Virtual memory (peak) " << virtualMemoryInfo.peak << "\n";
+    getMallocInfo().write(performanceLog);
 
 }
 
--- src/memoryInformation.hpp
+++ src/memoryInformation.hpp
@@ -1,12 +1,20 @@
 #pragma once
 
 #include <cstdint>
+#include "iosfwd.hpp"
 #include "string.hpp"
+
+
 
 namespace shasta2 {
 
     // Get peak virtual memory utilization of the current process, in bytes.
     uint64_t getPeakMemoryUsage();
+
+    // Get total physical memory available, in bytes.
+    uint64_t getTotalPhysicalMemory();
+
+
 
     // Get current and peak virtual memory utilization of the current process, in bytes.
     class VirtualMemoryInfo {
@@ -16,8 +24,25 @@ namespace shasta2 {
     };
     VirtualMemoryInfo getVirtualMemoryInfo();
 
-    // Get total physical memory available, in bytes.
-    uint64_t getTotalPhysicalMemory();
+
+
+    // Get statistics about memory allocated by malloc.
+    // mallinfo and mallinfo2 cannot be used as they
+    // return inaccurate/incomplete information.
+    class MallocInfo {
+    public:
+        uint64_t heapSize = 0;
+        uint64_t maxHeapSize = 0;
+        uint64_t mmapSize = 0;
+        uint64_t heapFree = 0;
+        uint64_t heapUsed() const
+        {
+            return heapSize - heapFree;
+        }
+        void write(ostream&) const;
+    };
+    MallocInfo getMallocInfo();
+
 
 
     void writeMemoryStatistics(const string&);
